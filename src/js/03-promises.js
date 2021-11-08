cfc5fa23d9270f27a34ef2a1dbf83c94242c0519
Dmytro Samyliak <84730425+dsamyliak@users.noreply.github.com>
2021-11-08 08:31:01 +0200

    Update 03-promises.js
    Add console.log to .then and .catch

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -48,9 +48,11 @@ function createPromise(position, delay) {
   promise
   .then(({ position, delay }) => {
       Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
       Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     });
 
 };
